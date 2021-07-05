4c058f01fa99bd77dcbb1d55ee47bc261629909a
Omkar <omkarhalgi50@gmail.com>
2021-07-05 20:11:42 +0530

    app added

---
+++ app.py
@@ -0,0 +1,68 @@
+import random
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///customer.sqlite3"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db=SQLAlchemy(app)
+
+class Cust(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    bank = db.Column(db.String(80))
+    transfers = db.Column(db.Integer)
+    Money = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<%r>' % self.name
+
+def createlistofcustomers():
+    if(Cust.query.all()==[]):
+        print(Cust.query.all())
+        list_of_names=['Ramesh','Abhinav','Virat','Shrikant','Kaushik','Vaibhav','Ajit','Tushar','Sakshi','Vinod','Viraj']
+        for i in range(10):
+            value=Cust(name=f'{list_of_names[i]}',email=f'{list_of_names[i]}@gmail.com',bank=f'bank{i+1}',transfers=random.randint(1,100),Money=random.randint(1000,10000))
+            db.session.add(value)
+            db.session.commit()
+@app.route('/')
+def bankingsystem():
+    return render_template('homepage.html')
+@app.route('/transfer',methods=['POST'])
+def transfer():
+    lst=list()
+    for i in Cust.query.all():
+        lst.append(i)
+    return render_template('alltransfers.html',store=lst)
+@app.route('/customers',methods=['POST','GET'])
+def customers():
+    createlistofcustomers()
+    if(request.form.get('sender',False)):
+        sender_id=request.form['sender']
+        receiver_id=request.form['receiver']
+        money=int(request.form['rupetext'])
+        sendname=Cust.query.filter_by(id=sender_id).first()
+        receivename=Cust.query.filter_by(id=receiver_id).first()
+        tra=sendname.transfers
+        print(sendname,receivename)
+        if(sendname.Money<money):
+            if(sendname.Money<0):
+                print(sendname,receivename)
+                print(send_pre_money,receie_pre_money,tra)
+                print('0 is executing')
+                sendname.Money=0
+                db.session.commit()
+        else:
+            sendname.transfers+=1
+            db.session.commit()
+            sendname.Money-=money
+            db.session.commit()
+            receivename.Money+=money
+            db.session.commit()
+    lst=list()
+    for i in Cust.query.all():
+        lst.append(i)
+    return render_template('viewallcust.html',store=lst)
+if __name__ == "__main__":
+    app.run(debug=True)
